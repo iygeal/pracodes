9d4399aac22a3eb1109f010714cd173ec87e0788
Innocent Anozie <iygeal@gmail.com>
2024-03-29 11:23:20 +0100

    Current HTML specs

--- cats_and_dogs.py
+++ cats_and_dogs.py
@@ -2,4 +2,16 @@
 """Module that reads cats and dogs files"""
 
 
-file_name = ""
+file_name = ['cats.txt, dogs.txt']
+
+def read_stuff(file_name):
+    """Function used to read stuff"""
+    try:
+        with open(file_name) as f_obj:
+            contents = f_obj.read()
+    except FileNotFoundError:
+        print(f"The file {file_name} doesn't extit")
+    else:
+        print(file_name)
+
+read_stuff(file_name)
