fe2475b7a63f659154492dd8f6042d4d1a565962
Innocent Anozie <iygeal@gmail.com>
2024-04-03 21:14:46 +0100

    Makefiles

---
+++ myproject/helper.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int helper(int a, int b)
+{
+	int my_sum;
+	
+	my_sum = (a + a);
+	return (my_sum);
+}
---
+++ myproject/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+	int outcome = helper(5, 4);
+	printf("%d\n", outcome);
+	return (0);
+}
