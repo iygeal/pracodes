8b079d720a02bdd13d605029c0e2d73280248f55
Innocent Anoziee <iygeal@gmail.com>
2024-10-02 18:03:43 +0100

    Saved unit test in JS

--- unittestJS/test/test_suite.js
+++ unittestJS/test/test_suite.js
@@ -1,14 +1,18 @@
-const assert = require('assert');
+import { expect } from 'chai';
 
-// Group tests using describe
-describe('Array', function () {
-  // Individual test case
-  it('should return -1 when the value is not present', function () {
-    assert.strictEqual([1, 2, 3].indexOf(4), -1);
+describe('Basic Arithmetic with Chai', function () {
+  it('should add two numbers correctly', function () {
+    const sum = 2 + 3;
+    expect(sum).to.equal(5); // Checks if the sum is 5
   });
 
-  // Another test case
-  it('should return the index when the value is present', function () {
-    assert.strictEqual([1, 2, 3].indexOf(1), 0);
+  it('should subtract two numbers correctly', function () {
+    const difference = 5 - 2;
+    expect(difference).to.equal(3); // Checks if the difference is 3
+  });
+
+  it('should include a number in the array', function () {
+    const arr = [1, 2, 3];
+    expect(arr).to.include(2); // Check if array includes the value 2
   });
 });
